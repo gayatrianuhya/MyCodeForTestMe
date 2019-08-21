4138d73f54399d29d6b0736f0dd782d1d93b52b5
gayatrianuhya <54269301+gayatrianuhya@users.noreply.github.com>
2019-08-21 11:11:10 +0530

    Update Myrunners.java

--- src/test/java/mystepdefinitions/Myrunners.java
+++ src/test/java/mystepdefinitions/Myrunners.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class )
-@CucumberOptions(features="C:\\Users\\Training_H2A.06.15\\eclipse-workspace\\cucumber123\\myfeatures\\demoontags.feature",glue="mystepdefinitions", tags= {"@Regression,@Smoke"},plugin={"html:target/cucumber-htmlreport.html","json:target/cucumber.json"})
+@CucumberOptions(features="C:\Users\Training_H2A.06.15\eclipse-workspace\cucumber123\myfeatures\Testmeparameters.feature",glue="mystepdefinitions",plugin={"html:target/cucumber-htmlreport.html","json:target/cucumber.json"})
 )
 public class Myrunners {
 	
